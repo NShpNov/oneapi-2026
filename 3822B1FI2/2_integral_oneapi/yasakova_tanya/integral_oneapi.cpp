ac3182d371a7a6ddec5087a5d07e86fba6bb945f
Tanya-Yasakova <yasakova_04@mail.ru>
2026-03-22 19:22:27 +0300

    fix 2

--- 3822B1FI2/2_integral_oneapi/yasakova_tanya/integral_oneapi.cpp
+++ 3822B1FI2/2_integral_oneapi/yasakova_tanya/integral_oneapi.cpp
@@ -2,34 +2,30 @@
 #include <cmath>
 
 float IntegralONEAPI(float start, float end, int count, sycl::device device) {
+  float result = 0.0f;
+  float delta = (end - start) / count;
+
+  {
+    sycl::buffer<float> buf_result(&result, 1);
     sycl::queue queue(device);
-    
-    float step = (end - start) / count;
-    float result = 0.0f;
-    
-    {
-        sycl::buffer<float> result_buf(&result, 1);
-        
-        queue.submit([&](sycl::handler& cgh) {
-            auto result_acc = result_buf.get_access<sycl::access::mode::write>(cgh);
-            
-            cgh.parallel_for(sycl::range<2>(count, count), [=](sycl::id<2> idx) {
-                int i = idx[0];
-                int j = idx[1];
-                
-                float x = start + (i + 0.5f) * step;
-                float y = start + (j + 0.5f) * step;
-                
-                float value = sycl::sin(x) * sycl::cos(y);
-                
-                sycl::atomic_ref<float, sycl::memory_order::relaxed,
-                                  sycl::memory_scope::device> atomic_result(result_acc[0]);
-                atomic_result.fetch_add(value * step * step);
-            });
-        });
-        
-        queue.wait();
-    }
-    
-    return result;
+
+    queue.submit([&](sycl::handler &cgh) {
+      auto reduction = sycl::reduction(buf_result, cgh, sycl::plus<float>());
+
+      cgh.parallel_for(sycl::range<2>(count, count), reduction,
+                       [=](sycl::id<2> idx, auto &sum) {
+                         int i = idx[0];
+                         int j = idx[1];
+
+                         float x = start + (i + 0.5f) * delta;
+                         float y = start + (j + 0.5f) * delta;
+
+                         sum += (sycl::sin(x) * sycl::cos(y));
+                       });
+    });
+
+    queue.wait();
+  }
+
+  return result * delta * delta;
 }
